979e344f8ae6ebc16091a1037404747a2d17fb68
spaceh3ad <jan.kwiatkowski@studenci.wsksim.edu.pl>
2021-04-30 01:14:20 +0200

    4. added tokensale

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,9 @@
-const TubblyToken = artifacts.require("TubblyToken.sol");
+const TubblyToken = artifacts.require("./TubblyToken.sol");
+const TubblySale = artifacts.require("./TubblySale.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(TubblyToken);
+  deployer.deploy(TubblyToken).then(function() {
+  	var tokenPrice =  1_000_000_000_000_000;
+	return deployer.deploy(TubblySale, TubblyToken.address, tokenPrice);
+  });
 };
